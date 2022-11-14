c0ee79bfb543c4becf1968e2706739eac338d190
chou <58171830+chou17@users.noreply.github.com>
2022-11-14 15:12:12 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,22 +1,22 @@
-#coding:utf-8
+# coding:utf-8
 
 class Config(object):
-    init_scale = 0.04
-    learning_rate = 0.001
-    max_grad_norm = 15
-    num_layers = 3
-    num_steps = 25 # number of steps to unroll the RNN for
-    hidden_size = 1000 # size of hidden layer of neurons
-    iteration = 40
-    save_freq = 5 #The step (counted by the number of iterations) at which the model is saved to hard disk.
-    keep_prob = 0.5
-    batch_size = 32
-    model_path = './model/Model' #the path of model that need to save or load
+    init_scale = 0.04                           #均勻分布的上下界
+    learning_rate = 0.001                       #學習率
+    max_grad_norm = 15                          #對梯度進行規範
+    num_layers = 3                              #RNN的層級數
+    num_steps = 25                              #每次訓練多少字
+    hidden_size = 1000                          #神經網路隱含層的維度
+    iteration = 40                              #模型迭代次數
 
-    #parameters for generation
-    save_time = 40 #load save_time saved models
-    is_sample = True #true means using sample, if not using max
-    is_beams = True #whether or not using beam search
-    beam_size = 2 #size of beam search
-    len_of_generation = 200 #The number of characters by generated
-    start_sentence = u'沒有菸抽的日子' #the seed sentence to generate text
+    save_freq = 5                               #每迭代多少次保存一次模型
+    keep_prob = 0.5                             #dropout的概率
+    batch_size = 32                             #min-batch的大小
+    model_path = './model/Model'                #model路徑
+  
+    save_time = 40                              #載入第幾次保存的模型
+    is_sample = True                            #是否使用sample，若False則是max
+    is_beams = True                             #是否使用beam-search解碼
+    beam_size = 2                               #beam-search的窗口大小
+    len_of_generation = 50                      #生成字數
+    start_sentence = u'他'                      #開始句子
