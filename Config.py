e625616c8f659cae9669afe7c5a52b6e88227433
chou <58171830+chou17@users.noreply.github.com>
2023-01-05 00:04:48 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,14 +7,14 @@ class Config(object):
     num_layers = 3                              #RNN的層級數
     num_steps = 25                              #每次訓練多少字
     hidden_size = 1000                          #神經網路隱含層的維度
-    iteration = 40                              #模型迭代次數
+    iteration = 500                              #模型迭代次數
 
     save_freq = 5                               #每迭代多少次保存一次模型
     keep_prob = 0.5                             #dropout的概率
     batch_size = 32                             #min-batch的大小
     model_path = './model/Model'                #model路徑
   
-    save_time = 40                              #載入第幾次保存的模型
+    save_time = 500                              #載入第幾次保存的模型
     is_sample = True                            #是否使用sample，若False則是max
     is_beams = True                             #是否使用beam-search解碼
     beam_size = 2                               #beam-search的窗口大小
